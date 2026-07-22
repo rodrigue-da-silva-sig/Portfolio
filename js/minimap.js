8726580eb9999a77db6ca25863226a3ed2275f55
Rodrigue da Silva <helvedasilva@gmail.com>
2026-07-22 16:42:49 +0200

    Update minimap.js

--- js/minimap.js
+++ js/minimap.js
@@ -34,7 +34,7 @@ fetch("js/TNS.geojson")
 
 var marker1 = L.marker([48.8703, 2.4897]).addTo(mapMini);
 marker1.bindPopup(`
-    <h3>Direction de l'Eau et de l'Assainissement de Seine-Saint-Denis (04/2024-09/2024)</h3></br>
+    <h3>Direction de l'Eau et de l'Assainissement de Seine-Saint-Denis (04/2024-09/2024)</h3>
     <p><strong>Mission:</strong>&nbsp;Accessiblité et valorisation du patrimoine d'eau de surface du département dans le cadre de la politique Seine-Saint-Denis, territoire d'eau&nbsp;</p>
     <p><strong>Zone d'étude:</strong>&nbsp;Seine-Saint-Denis (France)</p>
     <p><strong>Lieu de travail:</strong>&nbsp;Rosny-sous-Bois,Seine-Saint-Denis(93)</p>
@@ -42,7 +42,7 @@ marker1.bindPopup(`
            
 var marker2 = L.marker([2.15, 15.80]).addTo(mapMini);
 marker2.bindPopup(`
-     <h3>Fondation pour le Tri-National de la Sangha (02/2025-04/2026)</h3></br>
+     <h3>Fondation pour le Tri-National de la Sangha (02/2025-04/2026)</h3>
      <p><strong>Mission: Participation au projet scientifique CAPRO_TNS: ¨Problématique de l'économie cacaoyère à l'interface des aires protégées dans le Tri-National de la Sangha</strong>&nbsp;</p>
      <p><strong>Zone d'étude:</strong>&nbsp;Tri-National de la Sangha (Bassin du Congo)</p>
      <p><strong>Lieux de travail:</strong>&nbsp;Yaoundé, Mfoundi, Cameroun; Paris, Paris, France</p>
