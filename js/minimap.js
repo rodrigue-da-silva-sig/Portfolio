0c5333409267f5bc8b1a10b08c48641f059c0e6c
Rodrigue da Silva <helvedasilva@gmail.com>
2026-07-22 14:35:01 +0200

    Update minimap.js

--- js/minimap.js
+++ js/minimap.js
@@ -1,1 +1,17 @@
+const mapMini = L.map('mapMini', {
+    zoomControl: false,
+    dragging: false,
+    scrollWheelZoom: false,
+    doubleClickZoom: false,
+    boxZoom: false,
+    keyboard: false
+}).setView([20, 0], 2);
 
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        attribution: '&copy; OpenStreetMap'
+    }
+).addTo(mapMini);
+
+L.marker([48.8566, 2.3522]).addTo(mapMini);
