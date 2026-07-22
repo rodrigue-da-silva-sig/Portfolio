f4edc233b8dae9c022d8d88ba2a45e3fbb5877ee
Rodrigue da Silva <helvedasilva@gmail.com>
2026-07-22 17:29:40 +0200

    Update minimap.js

--- js/minimap.js
+++ js/minimap.js
@@ -40,16 +40,20 @@ fetch("js/TNS.geojson")
 var marker1 = L.marker([48.8703, 2.4897]).addTo(mapMini);
 marker1.bindPopup(`
     <h3>Expérience Professionnelle 1: Direction de l'Eau et de l'Assainissement de Seine-Saint-Denis (04/2024-09/2024)</h3>
-    <p><strong>Mission:</strong>&nbsp;Accessiblité et valorisation du patrimoine d'eau de surface du département dans le cadre de la politique Seine-Saint-Denis, territoire d'eau&nbsp;</p>
+    <p><strong>Projet:</strong>&nbsp;Accessiblité et valorisation du patrimoine d'eau de surface du département dans le cadre de la politique Seine-Saint-Denis, territoire d'eau&nbsp;</p>
     <p><strong>Zone d'étude:</strong>&nbsp;Seine-Saint-Denis (France)</p>
-    <p><strong>Lieu de travail:</strong>&nbsp;Rosny-sous-Bois,Seine-Saint-Denis (France)</p>
+    <a href="projects/experience1.html"><strong>
+        Voir les réalisations du projet
+    </strong></a>
 `);
            
 var marker2 = L.marker([2.15, 15.80]).addTo(mapMini);
 marker2.bindPopup(`
      <h3>Expérience Professionnelle 2: Fondation pour le Tri-National de la Sangha (02/2025-04/2026)</h3>
-     <p><strong>Mission:</strong>&nbsp;Participation au projet scientifique CAPRO_TNS: ¨Problématique de l'économie cacaoyère à l'interface des aires protégées dans le Tri-National de la Sangha</strong>&nbsp;</p>
+     <p><strong>Projet:</strong>&nbsp;Participation au projet scientifique CAPRO_TNS: ¨Problématique de l'économie cacaoyère à l'interface des aires protégées dans le Tri-National de la Sangha</strong>&nbsp;</p>
      <p><strong>Zone d'étude:</strong>&nbsp;Tri-National de la Sangha (Bassin du Congo)</p>
-     <p><strong>Lieux de travail:</strong>&nbsp;</br>-Yaoundé, Mfoundi (Cameroun)</br>-Paris, Paris (France)</p>
+     <a href="projects/experience1.html"><strong>
+        Voir les réalisations du projet
+    </strong></a>
 `);
 
